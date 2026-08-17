2c092320f22ad7c878a37a102c15f9b8a1d801b2
kana-shii <deiimos.mars@gmail.com>
2026-08-17 16:12:40 -0300

    .

---
+++ src/all/lezhin/build.gradle.kts
@@ -0,0 +1,22 @@
+import io.github.keiyoushi.gradle.api.ContentWarning
+
+plugins {
+    alias(kei.plugins.extension)
+}
+
+keiyoushi {
+    name = "Lezhin"
+    versionCode = 2
+    contentWarning = ContentWarning.MIXED
+    libVersion = "1.6"
+
+    source {
+        lang = "en"
+        baseUrl = "https://www.lezhinus.com"
+    }
+
+    source {
+        lang = "ko"
+        baseUrl = "https://www.lezhin.com"
+    }
+}
